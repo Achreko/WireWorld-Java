ab0b1a86f3654028c12bb5ad57a2cb95e6a775eb
kasiagrygorowicz <kasiagrygorowicz@interia.eu>
2020-05-02 15:13:59 +0200

    pierwsze proby z gui

---
+++ src/Frame.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Frame extends JFrame{
+//konstruktor
+    private Frame(){
+        super("Wire World");
+        setSize(400,200);
+        setResizable(true);
+        setLocationRelativeTo(null); //sprawi ze okno okienko pojawi sie na srodku
+        setVisible(true);//trzeba ustawic na true, inaczej nie bedzie widac okienka
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public static void myMainFrame(){
+        Frame frame=new Frame();
+        DialogBox box=new DialogBox(frame);
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+
+    public static void main (String[] args) {
+        Frame.myMainFrame();
+        System.out.println("chcesz zrobic "+DialogBox.g+" genearcjii");
+    }
+}
