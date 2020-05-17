408c38f258e3e4cbf160d50e60ae6b41a80781f6
kasiagrygorowicz <kasiagrygorowicz@interia.eu>
2020-05-17 14:03:37 +0200

    elementy

---
+++ src/Data.java
@@ -0,0 +1,21 @@
+public class Data {
+    public Element[] data =new Element[10];
+    public int number_of_elements=0;
+
+
+    private void resize(){
+        Element[] n=new Element[2*number_of_elements];
+        System.arraycopy(data,0,n,0, data.length);
+        data=n;
+    }
+
+    public void addElement(Element e){
+        if(number_of_elements == data.length) resize();
+        data[number_of_elements++]=e;
+    }
+
+}
+
+
+
+
---
+++ src/Element.java
@@ -0,0 +1,33 @@
+//przechowywanie kazdego elemementu(dioda,elektron itp) ktory poda uzytkownik w pliku
+public class Element {
+    private String name;
+    private String option;
+    private int x;
+    private int y;
+
+    public Element(String name,int x,int y){
+        this.x=x;
+        this.y=y;
+        this.name=name;
+    }
+
+    public Element(String name, String option,int x,int y){
+        this.name=name;
+        this.option=option;
+        this.x=x;
+        this.y=y;
+    }
+
+
+    @Override
+    public String toString() {
+        if(option!=null) {
+            StringBuilder b = new StringBuilder(name + " " + x + " " + y + " " + option);
+            return b.toString();
+        }else{
+            StringBuilder b = new StringBuilder(name + " " + x + " " + y );
+            return b.toString();
+        }
+
+    }
+}
