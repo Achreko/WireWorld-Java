719c53b0dabc741de477b1b3eaba8991863fb078
kasiagrygorowicz <kasiagrygorowicz@interia.eu>
2020-05-28 11:48:52 +0200

    getter

---
+++ src/Objects/Getter.java
@@ -0,0 +1,13 @@
+package Objects;
+
+public class Getter extends Body {
+
+    public static int get_rows(){
+        return rows;
+    }
+
+    public static int get_columns(){
+        return columns;
+    }
+
+}
